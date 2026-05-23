a07a78c26a313cd3d7c2253db09658e7e030539e
rudra91199 <rskrrudra9111999@gmail.com>
2026-05-23 23:43:56 +0600

    server up

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import app from "./app";
 import config from "./config/config";
 import { initDB } from "./db";
 
-const Main = () => {
+const main = () => {
   initDB();
   app.listen(config.port, () => {
     console.log(`Dev pulse is running on ${config.port} port`);
   });
 };
 
-Main();
+main();
