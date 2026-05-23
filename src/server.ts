68fbf26d4d4dc25bed73b85900be18d98a24b906
rudra91199 <rskrrudra9111999@gmail.com>
2026-05-23 23:41:59 +0600

    server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
-import app from "./app"
-import config from "./config/config"
-import { initDB } from "./db"
+import app from "./app";
+import config from "./config/config";
+import { initDB } from "./db";
 
 const Main = () => {
-    try {
-        initDB()
-        app.listen(config.port,() => {
-            console.log(`Dev pulse is running on ${config.port} port`)
-        })
-    } catch (error) {
-        console.error("Server Start Error", error)
-    }
-}
+  initDB();
+  app.listen(config.port, () => {
+    console.log(`Dev pulse is running on ${config.port} port`);
+  });
+};
 
-Main()
+Main();
